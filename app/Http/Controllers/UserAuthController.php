fcdc7ab7bce4cfcbaf4e73647210f07a1d0d88f6
feisalmombo <feisalmombo29@gmail.com>
2024-04-24 13:52:03 +0300

    add register methods at UserAuthController

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -1,10 +1,26 @@
 <?php
 
 namespace App\Http\Controllers;
+use App\Models\User;
 
 use Illuminate\Http\Request;
 
 class UserAuthController extends Controller
 {
-    //
+    public function register(Request $request)
+    {
+        $registerUserData = $request->validate([
+            'name'=>'required|string',
+            'email'=>'required|string|email|unique:users',
+            'password'=>'required|min:8'
+        ]);
+        $user = User::create([
+            'name' => $registerUserData['name'],
+            'email' => $registerUserData['email'],
+            'password' => Hash::make($registerUserData['password']),
+        ]);
+        return response()->json([
+            'message' => 'User Created ',
+        ]);
+    }
 }
