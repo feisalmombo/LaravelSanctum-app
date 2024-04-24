48355811aeb1e090bc064e1bc14c146d22159ae5
feisalmombo <feisalmombo29@gmail.com>
2024-04-24 14:18:38 +0300

    fix bugs

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -4,6 +4,9 @@ namespace App\Http\Controllers;
 use App\Models\User;
 
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Validator;
+use Illuminate\Support\Facades\Hash;
 
 class UserAuthController extends Controller
 {
