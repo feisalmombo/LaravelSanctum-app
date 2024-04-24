f4ead8a3d691750a5bcb86e54755ca10381c3772
feisalmombo <feisalmombo29@gmail.com>
2024-04-24 14:07:43 +0300

    fix bugs

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthController;
+use App\Http\Controllers\UserAuthController;
 
 /*
 |--------------------------------------------------------------------------
@@ -15,14 +15,13 @@ use App\Http\Controllers\AuthController;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
 
+Route::post('/register',[UserAuthController::class, 'register']);
 
-Route::post('register',[UserAuthController::class,'register']);
+Route::post('/login',[UserAuthController::class, 'login']);
 
-Route::post('login',[UserAuthController::class,'login']);
-
-Route::post('logout',[UserAuthController::class,'logout'])
+Route::post('/logout',[UserAuthController::class, 'logout'])
   ->middleware('auth:sanctum');
