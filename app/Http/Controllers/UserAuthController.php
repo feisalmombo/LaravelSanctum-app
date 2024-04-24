1a6dc5ca7fbb6fa23a18fb0870e5734788d2740f
feisalmombo <feisalmombo29@gmail.com>
2024-04-24 13:53:46 +0300

    add login methods at UserAuthController

--- app/Http/Controllers/UserAuthController.php
+++ app/Http/Controllers/UserAuthController.php
@@ -23,4 +23,22 @@ class UserAuthController extends Controller
             'message' => 'User Created ',
         ]);
     }
+
+
+    public function login(Request $request){
+        $loginUserData = $request->validate([
+            'email'=>'required|string|email',
+            'password'=>'required|min:8'
+        ]);
+        $user = User::where('email',$loginUserData['email'])->first();
+        if(!$user || !Hash::check($loginUserData['password'],$user->password)){
+            return response()->json([
+                'message' => 'Invalid Credentials'
+            ],401);
+        }
+        $token = $user->createToken($user->name.'-AuthToken')->plainTextToken;
+        return response()->json([
+            'access_token' => $token,
+        ]);
+    }
 }
